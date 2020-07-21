9e1c376c6686ee7f89605fe65966b39a2e47f9b3
Jungs <jungs6734@gmail.com>
2020-07-21 16:54:22 +0900

    Part1 (~42p)

--- TCPIPSocketPrograming/SocketPrograming/SocketPrograming/hello_server.cpp
+++ TCPIPSocketPrograming/SocketPrograming/SocketPrograming/hello_server.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <WinSock2.h>
 
 int main(int argc, char *argv[])
 {
+	WSADATA wsaData;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		_ERRHANDLING_H_("WSAStartup() error!");
 
 	return 0;
 }
