e8a773c3841ec823014d8c61f0eff02b85d953cf
Jungs <jungs6734@gmail.com>
2020-07-25 09:02:27 +0900

    Chapter3 - IP주소와 PORT번호

---
+++ 열혈TCPIPSocketPrograming/SocketPrograming/SocketPrograming/hello_server.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+void ErrorHandling(char* message);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAddr, clntAddr;
+
+	int szClntAddr;
+	char message[] = "Hello World!";
+
+	if (argc != 2)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		_ERRHANDLING_H_("WSAStartup() error!");
+
+	// 소켓 생성
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServSock == INVALID_SOCKET)
+		ErrorHandling("socket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	// IP주소와 PORT 번호의 할당
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("bind() error");
+
+	szClntAddr = sizeof(clntAddr);
+	// 클라이언트 프로그램에서의 연결요청을 수락할 때 호출하는 함수
+	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
+	if (hClntSock == INVALID_SOCKET)
+		ErrorHandling("accept() error");
+
+	send(hClntSock, message, sizeof(message), 0);
+	closesocket(hClntSock);
+	closesocket(hServSock);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
